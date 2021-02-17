0f5d87e34453002dc48906eed79f8bf763d3d65e
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-02-16 19:08:55 -0600

    created pseudo-code framework as comments to help with declarations, events, logic, and functions

--- develop/script.js
+++ develop/script.js
@@ -1,14 +1,28 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// START
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// WHEN button clicked (event)
+    // INPUT length-criteria prompt
 
-  passwordText.value = password;
+// WHEN next button clicked (event)
+    // IF condition 8 - 128 is FALSE
+        // THEN DISPLAY message "number must be between 8 - 128"
+        // WHEN ok button clicked (event)
+            // return to length-critera prompt
+    // ELSE condition 8 - 128 is TRUE
+        // THEN DISPLAY character-type prompt
 
-}
+// INPUT character-type prompt via checkbox list
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// WHEN next button clicked (event)
+    // IF condition > 1 max 4 boxes checked is FALSE (aka no boxes checked)
+        // THEN DISPLAY message "must check at least one box"
+        // WHEN ok button clicked (event)
+            // return to character-type prompt
+    // ELSE condition >1 mas 4 boxes checked is TRUE
+        // THEN DISPLAY criteria validation prompt
+
+// WHEN yes button clicked (event)
+    // DISPLAY password-message in webpage textarea
+        
+//WHEN no button clicked (event)
+    // DISPLAY oops-message in webpage textarea
