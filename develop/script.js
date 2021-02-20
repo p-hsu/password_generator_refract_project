aa4d708270641a0a678c8c481c7f921381f8fdc5
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-02-20 16:41:29 -0600

    edited pseudo code / markup variables and prompts for critera requireed

--- develop/script.js
+++ develop/script.js
@@ -1,33 +1,65 @@
-// START
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// WHEN button clicked (event)
-document.getElementById('generate').onclick = function() {
-    // INPUT length-criteria prompt
-    var userLength = Number(window.prompt("Please enter a number between 8 - 128:"));
+// Declare variables to be used in password generator sequence
+
+var special = [' !#$%&"()*+,-./:;<=>?@[]^_`{|}~'];
+var numbers = ["1234567890"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYX"];
+var userCriteria = [""];
+
+//Click button (event-type) to start prompts (event handler)
+generateBtn.addEventListener("click", genPswd);
+// DISPLAY length-criteria prompt for user input
+function genPswd() {
+  var userLength = window.prompt(
+    "Please enter a number  between 8 - 128 for the length of your password:"
+  );
+  // IF condition 8 - 128 FALSE (using operators and logic)
+  if (!(userLength >= 8) && userLength <= 128) {
+    // THEN DISPLAY alert  "number must be between 8 - 128"
+    window.alert("Number chosen MUST be between 8 and 128.");
+    // ELSE condition 8 - 128 is TRUE (using operators and logic)
+  } else {
+    //DISPLAY character-type confirmations
+    special = window.confirm("Do you want to include special characters?");
+    numbers = window.confirm("Do you want to include numbers?");
+    lowerCase = window.confirm("Do you want to include lower-case characters?");
+    upperCase = window.confirm("Do you want to include upper-case characters?");
+  }
+}
+// LOOP FOR each possible combination
+// none ==> ALERT  "Must choose at least one character-type criteria.""
+// all ==>
+// special
+// number
+// lowercase
+// uppercase
+// special + number
+// special + lowercase
+// special + uppercase
+// special + number + lowercase
+// special + number + uppercase
+// special + lowercase + uppercase
+// number + lowercase
+// number + uppercase
+// number + lowercase + uppercase
+// lowercase + uppercase
+
+// VALIDATE user selections "Are these criteria correct?"
+// IF condition FALSE (cancel button clicked)
+// THEN output "Oops, start again." in "Your Secure Password"
+// ELSE condition TRUE (ok button clicked)
+// THEN return  password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
-// WHEN ok button clicked (event)
-    // IF condition 8 - 128 is FALSE
-        // THEN DISPLAY message "number must be between 8 - 128"
-        // WHEN ok button clicked (event)s
-            // return to length-critera prompt
-    // ELSE condition 8 - 128 is TRUE
-        // THEN DISPLAY character-type prompt
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-// INPUT character-type prompt via checkbox list
-
-// WHEN next button clicked (event)
-    // IF condition >= 1 max 4 boxes checked is FALSE (aka no boxes checked)
-        // THEN DISPLAY message "must check at least one box"
-        // WHEN ok button clicked (event)
-            // return to character-type prompt
-    // ELSE condition >1 mas 4 boxes checked is TRUE
-        // THEN DISPLAY criteria validation prompt
-
-// WHEN submit button clicked (event)
-    // DISPLAY password-message in webpage textarea
-        
-//WHEN no button clicked (event)
-    // DISPLAY oops-message in webpage textarea
-
-// END 
+// END
