5c1ba5a7324a68a0462c3e340ea8bb576cafe5b2
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-02-21 14:51:40 -0600

    included function for random password generation

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Declare variables in global scope
 
 var special = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "| " ,"}", "~"];
@@ -8,19 +5,20 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var userCriteria;
-var userPassword = [];
+var userLength;
 var yesSpec;
 var yesNum;
 var yesLo;
 var yesUp;
+var userPassword = [];
+
+var generateBtn = document.querySelector("#generate");
 
 //Click button (event-type) to start prompts (event handler)
 generateBtn.addEventListener("click", genPswd);
 // DISPLAY length-criteria prompt for user input
 function genPswd() {
-  var userLength = window.prompt(
-    "Please enter a number  between 8 - 128 for the length of your password:"
-  );
+  userLength = parseInt(window.prompt("Please enter a number  between 8 - 128 for the length of your password:"));
       // IF condition 8 - 128 FALSE (no entry)
   if (!userLength) {
       // THEN DISPLAY alert "requires input"
@@ -28,7 +26,7 @@ function genPswd() {
   // IF condition 8 - 128 FALSE (outside parameters)
   } else if (!(userLength >= 8 && userLength <= 128)) {
     // THEN DISPLAY alert  "number must be between 8 - 128"
-    window.alert("Number chosen MUST be between 8 and 128.");
+    parseInt(window.alert("Number chosen MUST be between 8 and 128."));
     // ELSE condition 8 - 128 is TRUE (using operators and logic)
   } else {
     //DISPLAY character-type confirmations
@@ -39,12 +37,9 @@ function genPswd() {
   };
 
   // CONDITIONAL functions
-  // none ==> ALERT  "Must choose at least one character-type criteria.""
-  if (!(yesSpec && yesNum && yesLo && yesUp)) {
-    window.alert("You MUST choose at least one character-type.");
-  }
+
   // 4 character-types: use concat for results
-  else if (yesSpec && yesNum && yesLo && yesU) {
+  if (yesSpec && yesNum && yesLo && yesUp) {
     userCriteria = special.concat(numbers, lowerCase, upperCase);
   }
   // 3 character-types
@@ -102,15 +97,23 @@ function genPswd() {
   else if (yesUp) {
     userCriteria = uppercase;
   }
+  // none ==> ALERT  "Must choose at least one character-type criteria.""
+  else if (!yesSpec && !yesNum && !yesLo && !yesUp) {
+    userCriteria = window.alert("You MUST choose at least one character-type.");
+  };
 
-  // VALIDATE user selections "Are these criteria correct?"
+  // Random selection for user chosen variables
 
-// ELSE condition TRUE (ok button clicked)
-// THEN LOOP statement for random selection of variables
+  for (var i = 0; i < userLength; i++) {
+    var randomArr = userCriteria[Math.floor(Math.random() * userCriteria.length)];
+    userPassword.push(randomArr);
+  }
+  
+  return userPassword.join("");
 }
 // INPUTS password to textarea
 function writePassword() {
-  var password = generatePassword();
+  var password = genPswd();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
